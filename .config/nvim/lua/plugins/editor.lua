b0c22b1eb94119686b35fe888562e9eae4870557
BANG88 <sqibang@gmail.com>
2024-11-22 16:32:43 +0800

    feat: add terminal keymaps and toggleterm plugin configuration

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,22 @@
 return {
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
   { "echasnovski/mini.nvim", version = false },
   {
     "folke/tokyonight.nvim",
