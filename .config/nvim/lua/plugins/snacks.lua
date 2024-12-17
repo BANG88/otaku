712effdd91d11f62df2601f57e505ffff1d4503c
BANG88 <sqibang@gmail.com>
2024-12-17 13:14:45 +0800

    feat(dashboard): enhance dashboard sections with new configurations

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,36 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      sections = {
+        {
+          section = "terminal",
+          cmd = "chafa ~/.config/zoro.avif --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+          height = 16,
+          padding = 1,
+        },
+        { pane = 2, section = "keys", gap = 1, padding = 1 },
+        { pane = 1, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+
+        { pane = 2, gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { pane = 2, section = "startup" },
+      },
+    },
     indent = { enabled = false },
     notifier = {
       enabled = true,
