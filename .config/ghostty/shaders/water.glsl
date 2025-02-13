ca5d3b60c08662de6b7a18efb8560197d4e8c7d9
BANG88 <sqibang@gmail.com>
2025-02-13 18:55:12 +0800

    chore: add shaders

--- .config/ghostty/shaders/water.glsl
+++ .config/ghostty/shaders/water.glsl
@@ -1,74 +1,35 @@
-// adapted by Alex Sherwin for Ghostty from https://www.shadertoy.com/view/lljGDt
 
-#define BLACK_BLEND_THRESHOLD .4
-
-float hash21(vec2 p) {
-    p = fract(p * vec2(233.34, 851.73));
-    p += dot(p, p + 23.45);
-    return fract(p.x * p.y);
-}
-
-float rayStrength(vec2 raySource, vec2 rayRefDirection, vec2 coord, float seedA, float seedB, float speed)
-{
-    vec2 sourceToCoord = coord - raySource;
-    float cosAngle = dot(normalize(sourceToCoord), rayRefDirection);
-
-    // Add subtle dithering based on screen coordinates
-    float dither = hash21(coord) * 0.015 - 0.0075;
-
-    float ray = clamp(
-        (0.45 + 0.15 * sin(cosAngle * seedA + iTime * speed)) +
-        (0.3 + 0.2 * cos(-cosAngle * seedB + iTime * speed)) + dither,
-        0.0, 1.0);
-
-    // Smoothstep the distance falloff
-    float distFade = smoothstep(0.0, iResolution.x, iResolution.x - length(sourceToCoord));
-    return ray * mix(0.5, 1.0, distFade);
-}
+#define TAU 6.28318530718
+#define MAX_ITER 6
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+    vec3 water_color = vec3(1.0, 1.0, 1.0) * 0.5;
+	float time = iTime * 0.5+23.0;
 	vec2 uv = fragCoord.xy / iResolution.xy;
 
-	uv.y = 1.0 - uv.y;
-	vec2 coord = vec2(fragCoord.x, iResolution.y - fragCoord.y);
+    vec2 p = mod(uv*TAU, TAU)-250.0;
+	vec2 i = vec2(p);
+	float c = 1.0;
+	float inten = 0.005;
 
-	// Set the parameters of the sun rays
-	vec2 rayPos1 = vec2(iResolution.x * 0.7, iResolution.y * 1.1);
-	vec2 rayRefDir1 = normalize(vec2(1.0, 0.116));
-	float raySeedA1 = 36.2214;
-	float raySeedB1 = 21.11349;
-	float raySpeed1 = 1.1;
+	for (int n = 0; n < MAX_ITER; n++)
+	{
+		float t = time * (1.0 - (3.5 / float(n+1)));
+		i = p + vec2(cos(t - i.x) + sin(t + i.y), sin(t - i.y) + cos(t + i.x));
+		c += 1.0/length(vec2(p.x / (sin(i.x+t)/inten),p.y / (cos(i.y+t)/inten)));
+	}
+	c /= float(MAX_ITER);
+	c = 1.17-pow(c, 1.4);
+	vec3 color = vec3(pow(abs(c), 15.0));
+    color = clamp((color + water_color)*1.2, 0.0, 1.0);
 
-	vec2 rayPos2 = vec2(iResolution.x * 0.8, iResolution.y * 1.2);
-	vec2 rayRefDir2 = normalize(vec2(1.0, -0.241));
-	const float raySeedA2 = 22.39910;
-	const float raySeedB2 = 18.0234;
-	const float raySpeed2 = 0.9;
+    // perterb uv based on value of c from caustic calc above
+    vec2 tc = vec2(cos(c)-0.75,sin(c)-0.75)*0.04;
+    uv = clamp(uv + tc,0.0,1.0);
 
-	// Calculate the colour of the sun rays on the current fragment
-	vec4 rays1 =
-		vec4(1.0, 1.0, 1.0, 0.0) *
-		rayStrength(rayPos1, rayRefDir1, coord, raySeedA1, raySeedB1, raySpeed1);
-
-	vec4 rays2 =
-		vec4(1.0, 1.0, 1.0, 0.0) *
-		rayStrength(rayPos2, rayRefDir2, coord, raySeedA2, raySeedB2, raySpeed2);
-
-	vec4 col = rays1 * 0.5 + rays2 * 0.4;
-
-	// Attenuate brightness towards the bottom, simulating light-loss due to depth.
-	// Give the whole thing a blue-green tinge as well.
-	float brightness = 1.0 - (coord.y / iResolution.y);
-	col.r *= 0.05 + (brightness * 0.8);
-	col.g *= 0.15 + (brightness * 0.6);
-	col.b *= 0.3 + (brightness * 0.5);
-
-  vec2 termUV = fragCoord.xy / iResolution.xy;
-  vec4 terminalColor = texture(iChannel0, termUV);
-
-  float alpha = step(length(terminalColor.rgb), BLACK_BLEND_THRESHOLD);
-  vec3 blendedColor = mix(terminalColor.rgb * 1.0, col.rgb * 0.3, alpha);
-
-  fragColor = vec4(blendedColor, terminalColor.a);
+    fragColor = texture(iChannel0, uv);
+    // give transparent pixels a color
+    if ( fragColor.a == 0.0 ) fragColor=vec4(1.0,1.0,1.0,1.0);
+    fragColor *= vec4(color, 1.0);
 }
